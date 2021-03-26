406c86bce2f4b6694e948aee36961ee13f75e203
uncleankiwi <feldtspirit@gmail.com>
2021-03-26 11:45:28 -0400

    -

--- A4VG/Controllers/VisitController.cs
+++ A4VG/Controllers/VisitController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using A4VG.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,5 +13,63 @@ namespace A4VG.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Create()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        public ActionResult Create(Visit visit)
+		{
+            Context context = new Context();
+            context.Visits.Add(visit);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+		}
+
+        public ActionResult Details(int id)
+		{
+            return ViewFromId(id);
+		}
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+		{
+            return ViewFromId(id);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Visit visit)
+		{
+            Context context = new Context();
+            context.Entry(visit).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+		{
+            return ViewFromId(id);
+		}
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+		{
+            Context context = new Context();
+            Visit visit = context.Visits.Single(x => x.Id == id);
+            context.Visits.Remove(visit);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+		}
+
+        public ActionResult ViewFromId(int id)
+		{
+            Context context = new Context();
+            Visit visit = context.Visits.Single(x => x.Id == id);
+            return View(visit);
+		}
     }
 }
