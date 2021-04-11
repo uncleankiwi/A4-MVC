e9edc96e8bcd74ed8da778ac022b916ed19c38ca
uncleankiwi <feldtspirit@gmail.com>
2021-04-11 17:45:31 -0400

    -

---
+++ A4VG/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using A4VG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A4VG.Controllers
+{
+    public class LoginController : Controller
+    {
+		Context ctx = new Context();
+
+		[HttpGet]
+        public ActionResult Do()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public ActionResult Do(Admin admin)
+		{
+			try
+			{
+				List<Admin> loginMatches = ctx.Admins.Where(x => x.AdminName.Equals(admin.AdminName) && 
+				x.AdminPass.Equals(admin.AdminPass)).ToList();
+				
+				//if pass is correct, add cookie, redirect to home
+				if (loginMatches.Count == 1)
+				{
+					RedirectToAction("Index", "HomeController");
+				}
+				//if pass is incorrect, redirect to login, display error message
+				else
+				{
+
+				}
+			}
+			catch (Exception e)
+			{
+				System.Diagnostics.Debug.WriteLine(e.Message);
+			}
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ A4VG/Models/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace A4VG.Models
+{
+	[Table("tblAdmins")]
+	public class Admin
+	{
+		public int Id { get; set; }
+		[Display(Name = "Username")]
+		public String AdminName { get; set; }
+		[Display(Name = "Password")]
+		public String AdminPass { get; set; }
+		public String ErrorMessage { get; set; }
+	}
+}
