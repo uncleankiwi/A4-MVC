33dab92f54ae79365dfc3557345d132c4c539806
uncleankiwi <feldtspirit@gmail.com>
2021-04-11 18:02:00 -0400

    -

--- A4VG/Controllers/LoginController.cs
+++ A4VG/Controllers/LoginController.cs
@@ -7,40 +7,38 @@ using System.Web.Mvc;
 
 namespace A4VG.Controllers
 {
-    public class LoginController : Controller
-    {
+	public class LoginController : Controller
+	{
 		Context ctx = new Context();
 
 		[HttpGet]
-        public ActionResult Do()
-        {
-            return View();
-        }
+		public ActionResult Do()
+		{
+			return View();
+		}
 
 		[HttpPost]
 		public ActionResult Do(Admin admin)
 		{
 			try
 			{
-				List<Admin> loginMatches = ctx.Admins.Where(x => x.AdminName.Equals(admin.AdminName) && 
+				List<Admin> loginMatches = ctx.Admins.Where(x => x.AdminName.Equals(admin.AdminName) &&
 				x.AdminPass.Equals(admin.AdminPass)).ToList();
-				
+
 				//if pass is correct, add cookie, redirect to home
 				if (loginMatches.Count == 1)
 				{
 					RedirectToAction("Index", "HomeController");
 				}
-				//if pass is incorrect, redirect to login, display error message
-				else
-				{
-
-				}
 			}
+			//if pass is incorrect, redirect to login, display error message
 			catch (Exception e)
 			{
 				System.Diagnostics.Debug.WriteLine(e.Message);
 			}
-			return RedirectToAction("Index");
+			Admin errorResult = new Admin();
+			errorResult.ErrorMessage = "Username or password incorrect.";
+			return View(errorResult);
 		}
 	}
 }
